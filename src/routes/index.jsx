c850fa2347d9c0a630948c85682045d3edb2e16a
Leo Gomes <106993889+leo-gomes-dev@users.noreply.github.com>
2026-09-07 12:30:04 -0300

    feat: HashRouter no layout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "../components/Layout";
 import { Home } from "../pages/Home";
 import { Filme } from "../pages/Filme";
 
 export function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="filme" element={<Filme />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
